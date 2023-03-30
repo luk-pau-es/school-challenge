96f5e07ccf33ecc45ac0f6dc15d34b58bc75df36
Łukasz Pauszek <lukasz.pauszek@erlang-solutions.com>
2023-03-30 11:16:26 +0200

    Initial commit

---
+++ lib/school_challenge.ex
@@ -0,0 +1,18 @@
+defmodule SchoolChallenge do
+  @moduledoc """
+  Documentation for `SchoolChallenge`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SchoolChallenge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/school_challenge_test.exs
@@ -0,0 +1,8 @@
+defmodule SchoolChallengeTest do
+  use ExUnit.Case
+  doctest SchoolChallenge
+
+  test "greets the world" do
+    assert SchoolChallenge.hello() == :world
+  end
+end
