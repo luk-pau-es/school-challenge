a612a5267fa47a59097bfadc92681720e73ddb5f
mlokos <mlokos.github@gmail.com>
2023-04-01 09:27:30 +0200

    add garbage collector

--- lib/school_challenge/password_cracker_challenge.ex
+++ lib/school_challenge/password_cracker_challenge.ex
@@ -10,6 +10,8 @@ defmodule PasswordCrackerChallenge do
 
   def guess_password(hash), do: guess_password(hash, 0)
   def guess_password(hash, n) do
+    :erlang.garbage_collect()
+
     result = n..(n + 1) * @chunk_size
     |> Enum.map(&number_to_password(&1, ''))
     |> Enum.map(&check_password(&1, hash))
