4656290612035f790dc278de86143cabf88c2a83
mlokos <mlokos.github@gmail.com>
2023-04-01 11:46:51 +0200

    add correct chunking mechanism; support up to 64 cores

--- lib/school_challenge/password_cracker_challenge.ex
+++ lib/school_challenge/password_cracker_challenge.ex
@@ -7,14 +7,17 @@ defmodule PasswordCrackerChallenge do
   TIP: należy zwrócić uwagę, czy apostrofy są '' (lista znaków) czy "" (String, binary)
   """
   @chunk_size 320_000
+  @workload 5_000
 
   def guess_password(hash), do: guess_password(hash, 0)
   def guess_password(hash, n) do
     :erlang.garbage_collect()
 
     result = n * @chunk_size..(n + 1) * @chunk_size
-    |> Enum.map(&number_to_password(&1, ''))
-    |> Enum.map(&check_password(&1, hash))
+    |> Enum.chunk_every(@workload)
+    |> Enum.map(&(Task.async(fn -> check_list_of_numbers(&1, hash) end)))
+    |> Enum.map(&Task.await/1)
+    |> List.flatten()
     |> Enum.filter(&match?({true, _}, &1))
 
     if result == [] do
@@ -23,6 +26,17 @@ defmodule PasswordCrackerChallenge do
       Enum.at(result, 0)
       |> elem(1)
     end
+  end
+
+  defp check_list_of_numbers(numbers, hash) do
+    numbers
+    |> Enum.map(&check_number(&1, hash))
+  end
+
+  defp check_number(number, hash) do
+    number
+    |> number_to_password('')
+    |> check_password(hash)
   end
 
   defp number_to_password(0, ''), do: 'a'
