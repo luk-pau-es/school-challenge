64b164524e67cb1502997c4db78825a248878a28
Konrad P <konrad.przewloka@gmail.com>
2023-04-18 08:28:16 +0200

    some changes to hopefully speed up the task

--- lib/school_challenge/password_cracker_challenge.ex
+++ lib/school_challenge/password_cracker_challenge.ex
@@ -3,11 +3,11 @@ defmodule PasswordCrackerChallenge do
   Funkcja przyjmuje hash oryginalnego hasła
   Zwraca znalezione hasło na podstawie oryginalnego hasha
   jako listę znaków - np. 'abc'
-
   TIP: należy zwrócić uwagę, czy apostrofy są '' (lista znaków) czy "" (String, binary)
   """
 
   @max_number 141_167_095_653_376
+  @chunk_size 250_000
 
   def guess_password(hash), do: guess_password(hash, 0, 10_000_000)
 
@@ -21,7 +21,7 @@ defmodule PasswordCrackerChallenge do
 
     case result do
       [] -> guess_password(hash, max, max + 10_000_000)
-      a -> Enum.at(a, 0) |> elem(1)
+      a -> a
     end
   end
 
@@ -29,17 +29,16 @@ defmodule PasswordCrackerChallenge do
     stream =
       min..max
       |> Enum.to_list()
-      |> Enum.chunk_every(250_000)
-      |> Task.async_stream(fn subchunk ->
+      |> Enum.chunk_every(@chunk_size)
+      |> Task.async_stream(max_concurrency: 16, fn subchunk ->
         subchunk
         |> Enum.map(fn x -> generate_candidate(x, ~c"") end)
         |> Enum.map(fn y -> check_candidate(y, hash) end)
         |> Enum.filter(fn z -> match?({true, _}, z) end)
+        |> Enum.take(1) # stop processing as soon as a match is found
       end)
 
-    Enum.reduce(stream, [], fn {:ok, a}, acc -> [a | acc] end)
-    |> Enum.reject(&(&1 == []))
-    |> Enum.concat()
+    Enum.reduce(stream, [], fn {:ok, a}, acc -> a ++ acc end)
   end
 
   defp map_char(num) do
@@ -55,8 +54,26 @@ defmodule PasswordCrackerChallenge do
     generate_candidate(div(number, 27), char ++ password)
   end
 
+  defmodule Cache do
+    defstruct entries: %{}
+
+    def get_or_compute(cache, key, func) do
+      case Map.get(cache.entries, key) do
+        nil ->
+          result = func.()
+          cache = %{cache | entries: Map.put(cache.entries, key, result)}
+          {result, cache}
+        value ->
+          {value, cache}
+      end
+    end
+  end
+
   defp check_candidate(password, hash) do
-    candidate_hash = :crypto.hash(:sha512, password)
-    {hash == candidate_hash, password}
+    {result, _} = Cache.get_or_compute(%Cache{}, password, fn ->
+      candidate_hash = :crypto.hash(:sha512, password)
+      {hash == candidate_hash, password}
+      end)
+    result
   end
 end
